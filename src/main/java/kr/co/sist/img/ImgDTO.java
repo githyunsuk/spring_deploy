b0000a23b8f0ab53c8a1ef5ac690563ae4f519b5
githyunsuk <wngustjr1306@naver.com>
2025-07-25 09:43:51 +0900

    first commit

---
+++ src/main/java/kr/co/sist/img/ImgDTO.java
@@ -0,0 +1,19 @@
+package kr.co.sist.img;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImgDTO {
+
+	private String img;
+	private String title;
+	
+}
